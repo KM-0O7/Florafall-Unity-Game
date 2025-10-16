8204e88ec2c0957eec6d36b1378990487f6f0126
KM-0O7 <153666541+KM-0O7@users.noreply.github.com>
2025-10-16 11:20:14 -0400

    finished icicle falling platform

---
+++ Assets/Folliage/IciclePlatform/IcicleFallingPlatform.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using UnityEngine;
+
+public class IcicleFallingPlatform : MonoBehaviour
+{
+    Rigidbody2D rig;
+    private bool isfalling;
+    public float respawntime = 2f;
+    public float fallingtime = 1f;
+    private Vector2 basespawn;
+    BoxCollider2D collider2Dicicle;
+    Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        basespawn = transform.position;
+        rig = GetComponent<Rigidbody2D>();
+        rig.bodyType = RigidbodyType2D.Kinematic;
+        collider2Dicicle = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isfalling)
+        {
+            if (DruidFrameWork.canjump == true)
+            {
+                playercheck();
+                Debug.Log("falling = false");
+            }
+            
+        }
+    }
+
+    void playercheck()
+    {
+
+        Collider2D col = GetComponent<Collider2D>();
+
+        Vector2 checkPoint = new Vector2(transform.position.x, transform.position.y + col.bounds.extents.y + 0.1f);
+
+
+        Collider2D[] hits = Physics2D.OverlapPointAll(checkPoint);
+
+        foreach (var hit in hits)
+        {
+            if (hit.CompareTag("Player"))
+            {
+                StartCoroutine(platformfall());
+                break;
+            }
+        }
+    }
+
+    private IEnumerator platformfall()
+    {
+        isfalling = true;
+        yield return new WaitForSeconds(fallingtime);
+        rig.bodyType = RigidbodyType2D.Dynamic;
+        rig.gravityScale = 2f;
+        collider2Dicicle.enabled = false;
+        yield return new WaitForSeconds(respawntime);
+        animator.SetTrigger("respawn");
+        rig.bodyType = RigidbodyType2D.Static;
+        transform.position = basespawn;
+        rig.gravityScale = 0f;
+
+        yield return new WaitForSeconds(0.5f);
+        isfalling = false;
+        collider2Dicicle.enabled = true;
+    }
+}
