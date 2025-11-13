7f3b5566d1479883e93cae772b9fbfc2dda27e93
KM-0O7 <153666541+KM-0O7@users.noreply.github.com>
2025-11-12 21:58:51 -0500

    Updated spirit uI, fixed parallax on background

---
+++ Assets/UI/SpiritMoving.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SpiritFloat : MonoBehaviour
+{
+    public float amplitude = 2f;   
+    public float speed = 1f;      
+
+    private RectTransform rect;
+    private Image img;
+    private Vector2 startPos;
+
+    [SerializeField] private Sprite spiritOn;
+
+    void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        img = GetComponent<Image>();
+     
+        startPos = rect.anchoredPosition;
+        
+        float offset = Random.Range(0f, Mathf.PI * 2f);
+        StartCoroutine(FloatRoutine(offset));
+    }
+
+    private IEnumerator FloatRoutine(float offset)
+    {
+        while (true)
+        {
+            if (img.sprite == spiritOn)
+            {
+                float t = Time.time * speed + offset;
+                rect.anchoredPosition = startPos + new Vector2(0, Mathf.Sin(t) * amplitude);
+
+               
+            }
+            else
+            {
+                
+                rect.anchoredPosition = startPos;
+                
+            }
+
+            yield return null;
+        }
+    }
+}
