7d4732ca74bdbae7b1eb0b36b0c804bf3b6d6524
KM-0O7 <153666541+KM-0O7@users.noreply.github.com>
2026-02-09 11:40:54 -0500

    added zoom. and vignette increase for hidden areas

--- Assets/InteractableObjects/SecretRooms/HiddenAreaReveal.cs
+++ Assets/InteractableObjects/SecretRooms/HiddenAreaReveal.cs
@@ -1,13 +1,37 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.Tilemaps;
+
 
 public class HiddenAreaReveal : MonoBehaviour
 {
     [SerializeField] private float fadeTime = 0.75f;
     [SerializeField] private Tilemap hiddenTileMap;
-    private bool entered = false;
+    [SerializeField] private int zoomedInPPU = 40;
+    private Vignette vignette;
+    [SerializeField] private float vignetteIntensity;
+    private PixelPerfectCamera ppc;
 
+    private int startPPU;
+    private float startIntensity;
+    private bool entered = false;
+    private float startOrthoSize;
+    [SerializeField] private float zoomedInOrthoSize = 3.5f;
+
+
+    private void Awake()
+    {
+        Volume volume = Camera.main.GetComponent<Volume>();
+        ppc = Camera.main.GetComponent<PixelPerfectCamera>();
+        startPPU = ppc.assetsPPU;
+        startOrthoSize = Camera.main.orthographicSize;
+        if (volume.profile.TryGet(out vignette))
+        {
+            startIntensity = vignette.intensity.value;
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -17,7 +41,7 @@ public class HiddenAreaReveal : MonoBehaviour
             {
                 entered = true;
                 Debug.Log("Entered");
-                StartCoroutine(fade(0f));
+                StartCoroutine(fade(0f, vignetteIntensity, zoomedInPPU, zoomedInOrthoSize));
             }
         }
     }
@@ -29,23 +53,37 @@ public class HiddenAreaReveal : MonoBehaviour
             if (collision.gameObject.CompareTag("Player"))
             {
                 entered = false;
-                StartCoroutine(fade(1f));
+                StartCoroutine(fade(1f, startIntensity, startPPU, startOrthoSize));
+                vignette.intensity.value = startIntensity;
             }
         }
     }
 
-    private IEnumerator fade(float alpha)
+    private IEnumerator fade(float alpha, float endIntensity, int endPPU, float endOrtho)
     {
+        ppc.assetsPPU = ppc.assetsPPU;
+        ppc.enabled = false;
         Color startColor = hiddenTileMap.color;
         float startAlpha = startColor.a;
+        float intensity = vignette.intensity.value;
+        int startPPU = ppc.assetsPPU;
+        float startOrtho = Camera.main.orthographicSize;
         float t = 0f;
+
         while (t < fadeTime)
         {
             t += Time.deltaTime;
-            float a = Mathf.Lerp(startAlpha, alpha, t / fadeTime);
+            float k = t / fadeTime;
+            k = 1f - Mathf.Cos(k * Mathf.PI * 0.5f);
+            float a = Mathf.Lerp(startAlpha, alpha, k);
+            float i = Mathf.Lerp(intensity, endIntensity, k);
+            Camera.main.orthographicSize = Mathf.Lerp(startOrtho, endOrtho, k);
             yield return null;
+            vignette.intensity.value = i;
             hiddenTileMap.color = new Color(startColor.r, startColor.g, startColor.b, a);
         }
         hiddenTileMap.color = new Color(startColor.r,startColor.g,startColor.b, alpha);
+        ppc.assetsPPU = endPPU;
+        ppc.enabled = true;
     }
 }
