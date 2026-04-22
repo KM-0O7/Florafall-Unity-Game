0e8c2a07f8121fc3ffbe833e5766d4c7d60dfa89
KM-0O7 <153666541+KM-0O7@users.noreply.github.com>
2026-04-22 15:14:31 -0400

    added mushmimics jump attack

--- Assets/Enemies/GroundEnemies/MushMimic/MushMimic.cs
+++ Assets/Enemies/GroundEnemies/MushMimic/MushMimic.cs
@@ -6,17 +6,19 @@ public class MushMimic : MonoBehaviour, IGrowableEnemy, IEnemy
     private bool isGrown = false;
     private bool canDie = false;
     [SerializeField] private int cost;
-    EnemyDamage damage;
+    private EnemyDamage damage;
     public bool IsGrown => isGrown;
     public int spiritCost => cost;
     public bool Dead => damage.dead;
     public bool FlyingEnemy => false;
     private bool isLerping = false;
     public bool IsLerping => isLerping;
+
     public void SetLerp(bool value)
     {
         isLerping = value;
     }
+
     public bool GroundEnemy => true;
 
     public bool CanDie => canDie;
@@ -24,30 +26,34 @@ public class MushMimic : MonoBehaviour, IGrowableEnemy, IEnemy
     public float flashDuration = 0.3f;
 
     public float flashPeak = 1f;
-    [SerializeField] private float movementSpeed = 2f;
-    [SerializeField] private float jumpHeightCheck = 2f;
-    [SerializeField] private float jumpXCheck = 4f; 
+    [SerializeField] private float jumpDistanceCheck = 2f;
+    [SerializeField] private float attackCooldown = 5f;
+    private bool canHop = true;
     private bool canJump = true;
     private bool isJumping = false;
-    GameObject druid;
+    private GameObject druid;
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D enemyRig;
 
     [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float timeBetweenHops = 0.35f;
+    [SerializeField] private float jumpForce = 3f;
+    [SerializeField] private float jumpXForce = 2f;
 
     [SerializeField] private float pauseTime = 3f;
     [SerializeField] private float moveDistance = 5f;
-    [SerializeField] private float jumpForce = 2f;
     [SerializeField] private Transform checkPointPos;
-    Animator animator;
+    private BoxCollider2D collide;
+    [SerializeField] private GameObject platform;
+    private Animator animator;
 
     private bool movingright = true;
     private bool isPaused = false;
     private Vector2 startpos;
 
-
-    void Start()
+    private void Start()
     {
+        startpos = transform.position;
         spriteRenderer = GetComponent<SpriteRenderer>();
         druid = GameObject.FindGameObjectWithTag("Player");
         damage = GetComponent<EnemyDamage>();
@@ -56,14 +62,15 @@ public class MushMimic : MonoBehaviour, IGrowableEnemy, IEnemy
     }
 
     /* FIXED UPDATE
-   * Handles movement
-   * Handles pausing and starting at end via coroutine call
-   */
+     * Handles movement
+     * Handles pausing and starting at end via coroutine call
+     */
 
     private void FixedUpdate()
     {
-        if (!isGrown)
+        if (!isGrown && !isJumping)
         {
+            animator.SetFloat("XVelo", enemyRig.linearVelocityX);
             if (!damage.dead && !isPaused)
             {
                 float distanceFromStart = transform.position.x - startpos.x;
@@ -71,7 +78,11 @@ public class MushMimic : MonoBehaviour, IGrowableEnemy, IEnemy
                 if (movingright)
                 {
                     spriteRenderer.flipX = true;
-                    enemyRig.linearVelocityX = moveSpeed;
+                    if (canHop && !isPaused)
+                    {
+                        StartCoroutine(HopRoutine(1));
+                    }
+
                     if (distanceFromStart >= moveDistance)
                     {
                         StartCoroutine(PauseAtEnd(false));
@@ -80,7 +91,11 @@ public class MushMimic : MonoBehaviour, IGrowableEnemy, IEnemy
                 else
                 {
                     spriteRenderer.flipX = false;
-                    enemyRig.linearVelocityX = -moveSpeed;
+                    if (canHop && !isPaused)
+                    {
+                        StartCoroutine(HopRoutine(-1));
+                    }
+
                     if (distanceFromStart <= -moveDistance)
                     {
                         StartCoroutine(PauseAtEnd(true));
@@ -91,36 +106,81 @@ public class MushMimic : MonoBehaviour, IGrowableEnemy, IEnemy
             {
                 enemyRig.linearVelocityX = 0f;
             }
-
-            if ((druid.transform.position.y - gameObject.transform.position.y) > jumpHeightCheck && 
-                !isJumping && canJump && druid && Mathf.Abs(druid.transform.position.x - gameObject.transform.position.x) < jumpXCheck)
-            {
-                StartCoroutine(Jump());
-            }
-        }   
+        }
     }
 
+    private void Update()
+    {
+        //Attack
+        if (canJump)
+        {
+            if (Vector2.Distance(gameObject.transform.position, druid.transform.position) <= jumpDistanceCheck)
+            {
+                Debug.Log("Jumping");
+                StopAllCoroutines();
+                StartCoroutine(JumpAttack());
+            }
+        }
+    }
 
     /* COROUTINES
      * PauseAtEnd handles the mush pausing at the end of their roll
      */
 
-    private IEnumerator Jump()
+    private IEnumerator HopRoutine(int direction)
     {
-        isJumping = true;
-        canJump = false;
+        animator.SetBool("CanIdle", false);
+        canHop = false;
+        enemyRig.linearVelocityX = 0f;
+        animator.SetTrigger("Move");
+        yield return new WaitForSeconds(0.15f);
+        enemyRig.linearVelocityX = direction * moveSpeed;
+        yield return new WaitForSeconds(timeBetweenHops);
+        animator.SetBool("CanIdle", true);
+        enemyRig.linearVelocityX = 0f;
+        yield return new WaitForSeconds(1.5f);
+        canHop = true;
+    }
 
-        float direction = movingright ? 1f : -1f;
-        enemyRig.AddForce(new Vector2(direction, jumpForce), ForceMode2D.Impulse);
+    private IEnumerator JumpAttack()
+    {
+        var playerDir = druid.transform.position.x > gameObject.transform.position.x ? 1 : -1;
+        if (playerDir > 0)
+        {
+            spriteRenderer.flipX = true;
+            movingright = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+            movingright = false;
+        }
+        isJumping = true;
+        canHop = false;
+        canJump = false;
+        animator.SetTrigger("Attack");
+        yield return new WaitForSeconds(0.65f);
+        enemyRig.AddForce(new Vector2(jumpXForce * playerDir, jumpForce), ForceMode2D.Impulse);
+        yield return new WaitForSeconds(0.4f);
+        while (enemyRig.linearVelocityY > 0)
+        {
+            yield return null;
+            Debug.Log("Floating");
+        }
+        animator.SetTrigger("StopSpinning");
+        enemyRig.gravityScale = 3f;
         bool touchingGround = false;
         while (!touchingGround)
         {
             touchingGround = Physics2D.OverlapCircle(checkPointPos.position, 0.2f, LayerMask.GetMask("Ground"));
             yield return null;
         }
-        yield return new WaitForSeconds(1f);
+        enemyRig.gravityScale = 1;
+        animator.SetTrigger("Land");
+        yield return new WaitForSeconds(1.1f); 
+        canHop = true;
         isJumping = false;
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(attackCooldown);
         canJump = true;
     }
 
@@ -169,12 +229,24 @@ public class MushMimic : MonoBehaviour, IGrowableEnemy, IEnemy
 
     private IEnumerator GrowCycle()
     {
-        yield return null;
+        animator.SetTrigger("Grow");
+        isGrown = true;
+        yield return new WaitForSeconds(0.75f);
+        canDie = true;
+        platform.AddComponent<BoxCollider2D>();
+        collide = platform.GetComponent<BoxCollider2D>();
+        collide.enabled = true;
+        collide.usedByEffector = true;
     }
 
     private IEnumerator DieCycle()
     {
-        yield return null;
-    }
+        canDie = false;
+        animator.SetTrigger("Die");
+        Destroy(platform);
 
+        yield return new WaitForSeconds(3f);
+        animator.SetTrigger("dbdone");
+        isGrown = false;
+    }
 }
