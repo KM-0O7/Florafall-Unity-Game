195bb6b844a048834076e8d4762c73196df94ed7
KM-0O7 <153666541+KM-0O7@users.noreply.github.com>
2026-05-17 18:59:14 -0400

    added splitting to segments

--- Assets/Enemies/SegmentedEnemies/SawSnake/Segment.cs
+++ Assets/Enemies/SegmentedEnemies/SawSnake/Segment.cs
@@ -1,7 +1,24 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Segment : MonoBehaviour
 {
+    private EnemyDamage damage;
     public int segmentPosition = 0;
     public SegmentHead currentOwner;
+    private bool alreadyDead = false;
+
+    private void Start()
+    {
+        damage = GetComponent<EnemyDamage>();
+    }
+
+    private void Update()
+    {
+        if (damage.dead && !alreadyDead)
+        {
+            alreadyDead = true;
+            currentOwner.Split(segmentPosition);
+        }
+    }
 }
--- Assets/Enemies/SegmentedEnemies/SawSnake/SegmentHead.cs
+++ Assets/Enemies/SegmentedEnemies/SawSnake/SegmentHead.cs
@@ -8,8 +8,8 @@ public class SegmentHead : MonoBehaviour
     public List<Segment> segments = new List<Segment>();
     public List<EnemyDamage> segmentHealth = new List<EnemyDamage>();
     private List<Vector2> path = new List<Vector2>();
-    [SerializeField] private float segmentSpacing = 0.5f;
-    [SerializeField] private float moveDistForRecord = 0.5f;
+    private float segmentSpacing = 4f;
+    private float moveDistForRecord = 0.1f;
     private Vector2 lastRecordedPos;
     [SerializeField] private float detectionRange = 10f;
     private GameObject player;
@@ -17,9 +17,14 @@ public class SegmentHead : MonoBehaviour
 
     private void Start()
     {
+        StartUpSegment();
+    }
+    public void StartUpSegment()
+    {
         lastRecordedPos = transform.position;
         path.Add(transform.position);
         player = GameObject.FindGameObjectWithTag("Player");
+
         if (player != null)
         {
             playerTransform = player.GetComponent<Transform>();
@@ -28,43 +33,76 @@ public class SegmentHead : MonoBehaviour
 
     private void Update()
     {
-      
+        if (Vector2.Distance(transform.position, lastRecordedPos) >= moveDistForRecord)
+        {
             path.Insert(0, transform.position);
             lastRecordedPos = transform.position;
-        
+        }
 
         for (int i = 0; i < segments.Count; i++)
         {
             int pathIndex = Mathf.RoundToInt(i * segmentSpacing);
-
+            segments[i].segmentPosition = i;
             if (pathIndex < path.Count)
             {
-                segments[i].gameObject.transform.position = path[pathIndex];
-            }
-        }
-
-        if (Vector2.Distance(transform.position, playerTransform.position) <= detectionRange)
-        {
-        }
-
-        for (int i = 0; i < segmentHealth.Count; i++)
-        {
-            if (segmentHealth[i].Dead)
-            {
-                Debug.Log("Segment Died");
-                if (segments[i++] != null)
+                if (segments[i].currentOwner == gameObject.GetComponent<SegmentHead>())
                 {
-                    var newHead = segments[i++].AddComponent<SegmentHead>();
-                    newHead.segments.Clear();
-                    segments.Remove(segments[i]);
-
-                    for (int j = i++; j < segments.Count; j++)
-                    {
-                        newHead.segments.Add(segments[j]);
-                        segments.Remove(segments[j]);
-                    }
+                    segments[i].gameObject.transform.position = path[pathIndex];
                 }
             }
         }
+
+        //---- BEHAVIOUR ----
+        if (Vector2.Distance(transform.position, playerTransform.position) <= detectionRange)
+        {
+        }
+    }
+
+
+    //---- SPLITTING LOGIC ----
+
+    /* Void Split
+     * Split is a helper function that allows the segmented enemy to split
+     * Split will destroy the segment at split pos and separate the entire segemented enemy into multiple parts
+     * If there is no segments after the splitpos, it will simply destroy the segment
+     */
+    public void Split(int splitPos)
+    {
+        if (splitPos < 0 || splitPos >= segments.Count)
+        {
+            Debug.LogError("Invalid split position");
+            return;
+        }
+
+        Debug.Log("Split at " + splitPos);
+        Segment segmentToBeDestroyed = segments[splitPos];
+        int newHeadIndex = splitPos + 1;
+
+        if (newHeadIndex >= segments.Count)
+        {
+            Destroy(segmentToBeDestroyed.gameObject);
+            segments.RemoveAt(splitPos);
+            return;
+        }
+       
+        var headOfNewPack = segments[newHeadIndex].gameObject;
+
+        var newHead = headOfNewPack.AddComponent<SegmentHead>();
+        newHead.StartUpSegment();
+
+        for (int i = newHeadIndex; i < segments.Count; i++)
+        {
+            segments[i].currentOwner = newHead;
+            newHead.segments.Add(segments[i]);
+            newHead.segmentHealth.Add(segments[i].gameObject.GetComponent<EnemyDamage>());
+        }
+
+        for (int i = segments.Count - 1; i >= newHeadIndex; i--)
+        {
+            segmentHealth.RemoveAt(i);
+            segments.RemoveAt(i);
+        }
+        Destroy(segments[splitPos].gameObject);
+        segments.Remove(segments[splitPos]);
     }
 }
